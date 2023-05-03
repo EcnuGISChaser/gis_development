2d4894ae1585db4bea8d33483f3e688d1dadf1d3
ECNUGISChaser <107129163+EcnuGISChaser@users.noreply.github.com>
2023-05-03 08:29:01 +0800

    Update test.py

--- pages/test.py
+++ pages/test.py
@@ -8,7 +8,7 @@ Created on Wed Apr  5 19:32:40 2023
 import streamlit as st
 from PIL import Image
 
-image1 = Image.open(r'data\p1.jpg')
+image1 = Image.open(r'https://EcnuGISChaser.github.io/gis_development.github.io/data/p1.jpg')
 st.image([image1], 
          width = 400,
          caption=['my photo1'])
