0d0215afafc9c007c4f8450b71591f7ea9f5909a
ECNUGISChaser <107129163+EcnuGISChaser@users.noreply.github.com>
2023-05-11 12:25:17 +0800

    Update test.py

--- pages/test.py
+++ pages/test.py
@@ -5,7 +5,12 @@ Created on Wed Apr  5 19:32:40 2023
 @author: jpwu
 """
 
+from streamlit_keplergl import keplergl_static
+from keplergl import KeplerGl
 import geopandas as gpd
-url = "https://EcnuGISChaser.github.io/gis_development.github.io/data/shanghai_shops.json"
-gdf = gpd.read_file(url, driver='GeoJSON')
-gdf
+
+#data = r'c:\data\china\china_cities.geojson'
+#gdf = gpd.read_file(data)
+map_1 = KeplerGl(height=400)
+#map_1.add_data(gdf, 'china cities')
+keplergl_static(map_1,center_map=True)
