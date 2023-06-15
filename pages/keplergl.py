5409ce45105c3c3b35e87f7e716d2c77c3d1dc79
ECNUGISChaser <107129163+EcnuGISChaser@users.noreply.github.com>
2023-06-15 20:59:48 +0800

    Update keplergl.py

--- pages/keplergl.py
+++ pages/keplergl.py
@@ -5,14 +5,17 @@ Created on Wed Apr  5 19:32:40 2023
 @author: jpwu
 """
 
+import streamlit as st
 from streamlit_keplergl import keplergl_static
 from keplergl import KeplerGl
 import geopandas as gpd
-
-url = "https://EcnuGISChaser.github.io/gis_development.github.io/data/shanghai_shops.json"
-gdf = gpd.read_file(url)
-map_1 = KeplerGl(height=400)
-map_1.add_data(gdf, 'shanghai shops')
+st.title("天目山实习区POI点")
+data = r'https://EcnuGISChaser.github.io/gis_development/data/tms_POIs.geojson'
+gdf = gpd.read_file(data)
+config ={'version': 'v1',
+         'config': {'mapStyle':{'styleType':'satellite'}}}
+map_1 = KeplerGl(height=400,config=config)
+map_1.add_data(gdf, '天目山实习区POI点')
 keplergl_static(map_1,center_map=True)
 
 
