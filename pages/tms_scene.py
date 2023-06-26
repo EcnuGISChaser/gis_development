d57a96086ef8d912326e6c14ebd0566ce0e1c93f
ECNUGISChaser <joker_lord@sohu.com>
2023-06-26 16:27:15 +0800

    Create tms_scene.py

---
+++ pages/tms_scene.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 26 16:23:58 2023
+
+@author: jpwu
+"""
+
+import streamlit as st
+import streamlit.components.v1 as components
+
+# embed streamlit docs in a streamlit app
+components.iframe("https://www.geosceneonline.cn/geoscene/apps/Styler/index.html?appid=996de05a11d94aa891d82d4962e2d13d",
+                  height=800)
