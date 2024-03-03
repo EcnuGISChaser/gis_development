1ef9908b84cf214ddee6f288e22e364379b16bf1
ECNUGISChaser <107129163+EcnuGISChaser@users.noreply.github.com>
2024-03-03 15:34:25 +0800

    Update test.py

--- pages/test.py
+++ pages/test.py
@@ -8,6 +8,6 @@ Created on Wed Apr  5 19:32:40 2023
 import streamlit as st
 
 # Create the SQL connection to pets_db as specified in your secrets file.
-conn = st.experimental_connection('pets_db', type='sql')
-pet_owners = conn.query('select * from pet_owners')
-st.dataframe(pet_owners)
+# conn = st.experimental_connection('pets_db', type='sql')
+# pet_owners = conn.query('select * from pet_owners')
+# st.dataframe(pet_owners)
